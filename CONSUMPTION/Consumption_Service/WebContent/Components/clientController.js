99bb95b10f4d13b1fceb165819e6eb7e0a97edb1
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2022-05-15 04:19:29 +0530

    put ajax.. reading the value from table
    read value from table and fill it in the form..
    then use the same... add button to put the data..
    
    simple trick.. we change the functionality of the button.. based on the button clicked..
    if update button was clicked then ajax PUT  otherwise POST

--- CONSUMPTION/Consumption_Service/WebContent/Components/clientController.js
+++ CONSUMPTION/Consumption_Service/WebContent/Components/clientController.js
@@ -83,6 +83,30 @@ function onCompletionSaveConsumption(response, status)
 
 
 
+// UPDATE======================================================================
+$(document).on("click", ".btnUpdateConsumption", function(event)
+{
+		/* 
+		**.val() will set the value of that id
+		**closest("selector")- gets the first match with the selector..
+		**:eq(index)-- find the nth index
+		**.text() -- get the text 
+		*/
+
+		$("#Account_No").val($(this).closest("tr").find('td:eq(0)').text());
+		$("#Reading").val($(this).closest("tr").find('td:eq(1)').text());
+		$("#Date").val($(this).closest("tr").find('td:eq(2)').text());
+		
+		//reading attribute of the button to get the data-itemid
+		$("#Consumption_ID").val($(this).attr("data-itemid"));
+		
+		//when update button is clicked.. change the method to PUT from POST
+		$("#hideSaveBtn").val("Consumption_ID");
+	
+	
+		
+});
+
 
 // ==============CLIENT- model validation ===================
 function consumptionFormValidation()
